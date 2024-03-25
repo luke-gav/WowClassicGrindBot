9152cbe711e514ecae9bab5269f431efb529f7b1
Xian55 <367101+Xian55@users.noreply.github.com>
2024-03-25 23:46:08 +0100

    Core: PathDrawer: Cleanup and Tidyup code magic values are not constant variables. Added validation againts World coordinates.

--- Core/PathDrawer/PathDrawer.cs
+++ Core/PathDrawer/PathDrawer.cs
@@ -12,111 +12,125 @@ namespace Core;
 
 public static class PathDrawer
 {
-    private const int minSize = 40;
+    private const int BORDER_SIZE = 40;
 
-    private const float mapScalar = 100f;
-    private const float radius = 3f;
+    private const int MIN_SIZE = 200;
+    private const int UPSCALE_BELOW_MIN_SIZE = 2;
+
+    private const float MAP_SCALAR = 100f;
+    private const float RADIUS = 3f;
 
     public static void Execute(
-        List<Vector3> path, string imgUrl, string output)
+        List<Vector3> mapPath, string imgUrl, string outputPath)
     {
-        List<PointF> points = new(path.Count);
-        points.AddRange(path.ConvertAll(
-            p => new PointF(p.X / mapScalar, p.Y / mapScalar)));
+        if (!ValidMapCoordinates(mapPath))
+            return;
 
-        Bitmap background = DownloadImageAsBitmap(imgUrl);
+        List<PointF> points = new(mapPath.Count);
 
-        using Graphics gr = Graphics.FromImage(background);
-        gr.InterpolationMode = InterpolationMode.HighQualityBicubic;
-        gr.CompositingQuality = CompositingQuality.HighQuality;
+        points.AddRange(mapPath.ConvertAll(
+            p => new PointF(p.X / MAP_SCALAR, p.Y / MAP_SCALAR)));
+
+        Bitmap background = DownloadBitmap(imgUrl);
+
+        using Graphics g = Graphics.FromImage(background);
+        g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+        g.CompositingQuality = CompositingQuality.HighQuality;
 
         float width = background.Width;
         float height = background.Height;
-        DrawPath(gr, points, width, height, radius);
+        DrawPath(g, points, width, height, RADIUS);
 
-        RectangleF sRect = RecalculateBounds(points);
-        sRect.X *= width;
-        sRect.Y *= height;
-        sRect.Width *= width;
-        sRect.Height *= height;
-
-        sRect.Inflate(minSize, minSize);
-
-        sRect.X = MathF.Max(sRect.X, 0);
-        sRect.Y = MathF.Max(sRect.Y, 0);
-
-        if (sRect.X + sRect.Width > width)
-            sRect.Width = width - sRect.X;
-        if (sRect.Y + sRect.Height > height)
-            sRect.Height = height - sRect.Y;
-
-        // Draw starting pos to the bottom
-        Rectangle rect = Rectangle.Round(sRect);
+        Rectangle rect = CalculateBounds(points, width, height);
 
         Font font = SystemFonts.DefaultFont;
 
         // Black background
         PointF startPoint = points.First();
         string startText =
-            $"{startPoint.X * mapScalar:0.##} " +
-            $"{startPoint.Y * mapScalar:0.##}";
+            $"{startPoint.X * MAP_SCALAR:0.##} " +
+            $"{startPoint.Y * MAP_SCALAR:0.##}";
 
-        SizeF sizeStart = gr.MeasureString(startText, font);
+        SizeF sizeStart = g.MeasureString(startText, font);
 
-        Rectangle infoRect = new(
-            new Point(rect.X, rect.Bottom - (int)sizeStart.Height),
-            new Size(rect.Width, (int)sizeStart.Height));
+        Rectangle bottomInfoPanelRect = new(
+            new(rect.X, rect.Bottom - (int)sizeStart.Height),
+            new(rect.Width, (int)sizeStart.Height));
 
-        gr.FillRectangle(Brushes.Black, infoRect);
+        g.FillRectangle(Brushes.Black, bottomInfoPanelRect);
 
         // xx.xx yy.yy
         Rectangle startRect = new(
-            new Point(infoRect.X, infoRect.Y),
-            new Size((int)(sizeStart.Width + 1), infoRect.Height));
+            new(bottomInfoPanelRect.X, bottomInfoPanelRect.Y),
+            new((int)(sizeStart.Width + 1), bottomInfoPanelRect.Height));
 
-        gr.DrawString(startText, font, Brushes.White, startRect);
+        g.DrawString(startText, font, Brushes.White, startRect);
 
-        Bitmap outputImg =
+        Bitmap output =
             background.Clone(rect, PixelFormat.Format32bppArgb);
 
-        if (outputImg.Width < 200 || outputImg.Height < 200)
+        if (output.Width < MIN_SIZE || output.Height < MIN_SIZE)
         {
-            const int upscale = 2;
+            Bitmap scaled = new(output,
+                output.Width * UPSCALE_BELOW_MIN_SIZE,
+                output.Height * UPSCALE_BELOW_MIN_SIZE);
 
-            Bitmap scaled = new(outputImg,
-                outputImg.Width * upscale,
-                outputImg.Height * upscale);
-
-            outputImg.Dispose();
-            outputImg = scaled;
+            output.Dispose();
+            output = scaled;
         }
 
-        ImageCodecInfo encoder = ImageCodecInfo.GetImageEncoders().
+        ImageCodecInfo jpgEncoder = ImageCodecInfo.GetImageEncoders().
             First(c => c.FormatID == ImageFormat.Jpeg.Guid);
 
         EncoderParameters encParams = new()
         {
-            Param = new[] {
+            Param = [
                 new EncoderParameter(Encoder.Quality, 100L)
-            }
+            ]
         };
 
-        outputImg.Save(output, encoder, encParams);
+        output.Save(outputPath, jpgEncoder, encParams);
     }
 
-    private static RectangleF RecalculateBounds(List<PointF> list)
+    private static bool ValidMapCoordinates(List<Vector3> mapPath)
+    {
+        return !mapPath.Any(p =>
+            p.X < 0 || p.X > MAP_SCALAR ||
+            p.Y < 0 || p.Y > MAP_SCALAR);
+    }
+
+    private static Rectangle CalculateBounds(
+        List<PointF> list, float width, float height)
     {
         float minX = list.Min(p => p.X);
         float minY = list.Min(p => p.Y);
         float maxX = list.Max(p => p.X);
         float maxY = list.Max(p => p.Y);
 
-        return new(
-            new PointF(minX, minY),
-            new SizeF(maxX - minX, maxY - minY));
+        RectangleF rect = new(
+            new(minX, minY),
+            new(maxX - minX, maxY - minY));
+
+        rect.X *= width;
+        rect.Y *= height;
+        rect.Width *= width;
+        rect.Height *= height;
+
+        rect.Inflate(BORDER_SIZE, BORDER_SIZE);
+
+        rect.X = MathF.Max(rect.X, 0);
+        rect.Y = MathF.Max(rect.Y, 0);
+
+        if (rect.X + rect.Width > width)
+            rect.Width = width - rect.X;
+
+        if (rect.Y + rect.Height > height)
+            rect.Height = height - rect.Y;
+
+        return Rectangle.Round(rect);
     }
 
-    private static Bitmap DownloadImageAsBitmap(string url)
+    private static Bitmap DownloadBitmap(string url)
     {
         using HttpClient httpClient = new();
         using HttpRequestMessage request = new(HttpMethod.Get, url);
@@ -125,21 +139,22 @@ public static class PathDrawer
         return new(response.Content.ReadAsStream());
     }
 
-    private static void DrawPath(Graphics gr,
+    private static void DrawPath(Graphics g,
         List<PointF> list, float width, float height, float radius)
     {
         for (int i = list.Count - 1; i >= 0; i--)
         {
             float shade = (float)i / list.Count;
             int step = 255 - (int)(255 * shade);
-            using SolidBrush b =
-                new(Color.FromArgb(255, step, 0, 0));
+            Color redShade = Color.FromArgb(255, step, 0, 0);
+
+            using SolidBrush brush = new(
+                i == 0
+                ? Color.White
+                : redShade);
 
             PointF p = list[i];
-            gr.FillEllipse(
-                p == list.First()
-                ? Brushes.White
-                : b,
+            g.FillEllipse(brush,
                 width * p.X, height * p.Y, radius, radius);
         }
     }

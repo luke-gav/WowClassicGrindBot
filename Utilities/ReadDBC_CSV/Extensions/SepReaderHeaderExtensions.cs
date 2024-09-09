2ece7d2bbd64932c5d5d4a455ab27a930557372f
Xian55 <367101+Xian55@users.noreply.github.com>
2024-09-10 01:21:26 +0200

    Utilities: ReadDBC_CSV: Set Central package management - Update NugetPackages - Refactor SepHeaderExtension

---
+++ Utilities/ReadDBC_CSV/Extensions/SepReaderHeaderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using nietras.SeparatedValues;
+
+namespace ReadDBC_CSV;
+
+internal static class SepReaderHeaderExtensions
+{
+    public static int IndexOf(this SepReaderHeader sep, string key1, string key2)
+    {
+        return
+            sep.TryIndexOf(key1, out var colIndex)
+            ? colIndex
+            : sep.IndexOf(key2);
+    }
+
+    public static int IndexOf(this SepReaderHeader sep, string key, int index)
+    {
+        return
+            sep.TryIndexOf(key, out var colIndex)
+            ? colIndex
+            : sep.IndexOf(sep.ColNames[index]);
+    }
+}
